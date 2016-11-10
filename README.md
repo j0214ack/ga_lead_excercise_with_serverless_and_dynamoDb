b3d452980124ffd7d94ea913c2cf69de7b542747
陳展佑 <chenzhanyou@HHGde-MacBook-Air.local>
2016-11-10 11:24:58 +0800

    finally tamed api codes

---
+++ README.md
@@ -0,0 +1,5 @@
+# Serverless DynamoDB excercise
+This project is for serverless DynamoDB excercise.
+It
+1. Recieves google clientId and client information and stores it in dynamoDB.
+2. Expose functionalaity to update information based on google clientId back to Google Analytics.
--- handler.js
+++ handler.js
@@ -1,16 +1,78 @@
-'use strict';
+const AWS = require('aws-sdk'); // eslint-disable-line
+const uuid = require('node-uuid');
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+function display(object) {
+  return JSON.stringify(object, null, 2);
+}
+
+module.exports.leads = (event, context, callback) => {
+  console.log('Event: ', display(event));
+  console.log('Context: ', display(context));
+  console.log('event recieved');
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+  console.log('db client created');
+  const done = (err, res) => {
+    if (err) {
+      console.error(err);
+    }
+    callback(null, {
+      statusCode: err ? '400' : '200',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: err ? display(err) : JSON.stringify(res),
+    });
   };
+  switch (event.httpMethod) {
+    case 'GET':
+      console.log('GET method detected');
+      dynamoDb.scan({ TableName: 'ga_lead_excercise' }, done);
+      break;
+    case 'POST': {
+      console.log('POST method detected');
+      const item = JSON.parse(event.body).payload.Item;
+      item.id = uuid.v1();
+      dynamoDb.put({
+        TableName: 'ga_lead_excercise',
+        Item: item,
+      }, done);
+      break;
+    }
+    case 'PUT':
+      dynamoDb.get({
+        TableName: 'ga_lead_excercise',
+        Key: {
+          id: event.queryStringParameters.id,
+        },
+      }, (err) => {
+        if (err) {
+          done(err);
+        } else {
+          const item = JSON.parse(event.body).payload.Item;
+          item.id = event.queryStringParameters.id;
+          dynamoDb.put({
+            TableName: 'ga_lead_excercise',
+            Item: item,
+          }, done);
+        }
+      });
+      break;
+    case 'DELETE':
+      dynamoDb.delete({
+        TableName: 'ga_lead_excercise',
+        Key: {
+          id: event.queryStringParameters.id,
+        },
+      }, done);
+      break;
+    default:
 
-  callback(null, response);
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.ga = (event, context) => {
+  console.log('Event: ', display(event));
+  console.log('Context: ', display(context));
 };
