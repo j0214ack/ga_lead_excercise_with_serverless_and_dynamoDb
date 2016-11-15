f0f8c9d412eeff0e821ef53717270e4e8ff779f5
陳展佑 <chenzhanyou@HHGde-MacBook-Air.local>
2016-11-15 16:09:00 +0800

    Added cors. Added emptyString cleaner before putting into dynamoDB

--- handler.js
+++ handler.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint no-param-reassign: ["error", { "props": false }] */
 const AWS = require('aws-sdk'); // eslint-disable-line
 const uuid = require('node-uuid');
 
@@ -5,12 +7,22 @@ function display(object) {
   return JSON.stringify(object, null, 2);
 }
 
+function removeEmptyStringElements(obj) {
+  for (var prop in obj) { // eslint-disable-line
+    if (typeof obj[prop] === 'object') { // dive deeper in
+      removeEmptyStringElements(obj[prop]);
+    } else if (obj[prop] === '') { // delete elements that are empty strings
+      delete obj[prop];
+    }
+  }
+  return obj;
+}
+
 module.exports.leads = (event, context, callback) => {
   console.log('Event: ', display(event));
   console.log('Context: ', display(context));
-  console.log('event recieved');
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
-  console.log('db client created');
+
   const done = (err, res) => {
     if (err) {
       console.error(err);
@@ -19,19 +31,21 @@ module.exports.leads = (event, context, callback) => {
       statusCode: err ? '400' : '200',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
       body: err ? display(err) : JSON.stringify(res),
     });
   };
+
   switch (event.httpMethod) {
     case 'GET':
-      console.log('GET method detected');
       dynamoDb.scan({ TableName: 'ga_lead_excercise' }, done);
       break;
     case 'POST': {
-      console.log('POST method detected');
-      const item = JSON.parse(event.body).payload.Item;
+      const item = removeEmptyStringElements(JSON.parse(event.body).payload.Item);
       item.id = uuid.v1();
+      item.createdAt = Date.now();
+      console.log(item);
       dynamoDb.put({
         TableName: 'ga_lead_excercise',
         Item: item,
@@ -48,7 +62,7 @@ module.exports.leads = (event, context, callback) => {
         if (err) {
           done(err);
         } else {
-          const item = JSON.parse(event.body).payload.Item;
+          const item = removeEmptyStringElements(JSON.parse(event.body).payload.Item);
           item.id = event.queryStringParameters.id;
           dynamoDb.put({
             TableName: 'ga_lead_excercise',
